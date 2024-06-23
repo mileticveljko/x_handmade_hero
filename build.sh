02d659adad7ed4d1572951fac13ba42593b6f40e
Dzo <mileticveljko99@gmail.com>
2024-06-23 13:04:38 +0200

    Project setup

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Variables
+CXX="g++"
+SRC="../src/platform/xcb_handmade_hero.cpp"
+OUTPUT="handmade_hero"
+CFLAGS="-std=c++11 -g -O0 -Wall -Wextra -pedantic"
+LDFLAGS="-lxcb -lX11 -lX11-xcb"
+DEFINES="-DDEBUG"
+
+# Disable pushd and popd echoing
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+
+# Create bin folder and enter it
+mkdir -p bin
+pushd bin
+
+# Compile command
+$CXX $SRC $CFLAGS $LDFLAGS -o $OUTPUT
+
+# Leave bin folder
+popd
---
+++ src/platform/xcb_handmade_hero.cpp
@@ -0,0 +1,20 @@
+#include <X11/Xlib.h>
+#include <X11/Xlib-xcb.h>
+#include <xcb/xcb.h>
+
+#define UNUSED(x) (void)x
+
+int main(int argc, char* argv[])
+{
+	UNUSED(argc);
+  UNUSED(argv);
+
+	Display *display = XOpenDisplay(nullptr);
+	xcb_connection_t *connection = XGetXCBConnection(display);
+
+	UNUSED(connection);
+  
+  XCloseDisplay(display);
+	
+  return 0;
+} 
